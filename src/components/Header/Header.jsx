f54d81b4b1f7fbcccf34d6faa2bdaadec77ba814
GeffersonST <geffsevero@gmail.com>
2021-11-29 22:11:17 -0300

    Estruturando

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="container"></header>
+      <header className="headerGroup">
+        <header className="logo">Logo</header>
+        <navbar>
+          <h1>Home</h1>
+          <h1>Lista de Pokémons</h1>
+          <h1>Contato</h1>
+        </navbar>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
