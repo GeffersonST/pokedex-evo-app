5ca7197cf30983012a0a328a15b72075f143ea04
GeffersonST <geffsevero@gmail.com>
2021-12-01 19:31:44 -0300

    Consumir API

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 
 import Home from "./Pages/Home/Home";
 import Header from "./components/Header/Header";
+import ViewPokemon from "./Pages/ViewPokemon/ViewPokemon";
 function App() {
   return (
     <div>
       <Header />
+
       <Home />
+      <ViewPokemon />
     </div>
   );
 }
--- src/Pages/ViewPokemon/ViewPokemon.js
+++ src/Pages/ViewPokemon/ViewPokemon.js
@@ -1,5 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
+
 const ViewPokemon = () => {
-  return <div></div>;
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      await axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
+        console.log(response.data);
+        setPokemonData(response.data.results);
+      });
+    };
+
+    getPokemon();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        <h1>Pokedex</h1>
+
+        <li>
+          {pokemonData.map((pokemon) => {
+            return <li>{pokemon.name}</li>;
+          })}
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default ViewPokemon;
